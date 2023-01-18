8ad55b03b87e38a2a4df623e9c9c3f88562429a0
Aaront028 <info@aarontan.co.nz>
2023-01-19 11:24:13 +1300

    Add project files.

---
+++ NumberGuesser/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Set app vars
+string appName = "Number Guesser";
+string appVersion = "1.0.0";
+string appAuthor = "Brad Traversy";
+
+// Change text color
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+
+// Write out app info
+Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+
+// Reset console color
+Console.ResetColor();
+
+// Ask users name
+Console.WriteLine("What is your name?");
+string name  = Console.ReadLine();
+
